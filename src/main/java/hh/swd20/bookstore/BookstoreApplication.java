4b209721dc99309f319bd35cd438857b180f79db
KingHenx <henkka.rossi@gmail.com>
2020-09-29 14:23:03 +0300

    Category added

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
+import hh.swd20.bookstore.domain.Category;
+import hh.swd20.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -16,16 +18,25 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return(args) ->{
 			Book a = new Book("Lord of the Flies", "Willian Golding", 1954, "0-571-05686-5", 20);
 			Book b = new Book("The Egyptian", "Mika Waltari", 1945, "1-55652-441-2", 30);
 			Book c = new Book("Purge", "Sofi Oksanen", 2010, "951-0-33973-3", 25);
 			
-			repository.save(a);
-			repository.save(b);
-			repository.save(c);
+			bookRepository.save(a);
+			bookRepository.save(b);
+			bookRepository.save(c);
+			
+			Category x = new Category("Horror");
+			Category y = new Category("Romance");
+			Category z = new Category("Sci-Fi");
+			
+			categoryRepository.save(x);
+			categoryRepository.save(y);
+			categoryRepository.save(z);
 		};
 	}
+
 
 }
