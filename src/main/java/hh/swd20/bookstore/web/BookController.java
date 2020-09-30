1805bf661a314b3123e170b7bc38c3498ead03ff
KingHenx <henkka.rossi@gmail.com>
2020-09-30 16:29:14 +0300

    Category adding works

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -11,18 +11,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
+import hh.swd20.bookstore.domain.Category;
+import hh.swd20.bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
 
 	@Autowired
-	private BookRepository repository;
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@RequestMapping("/index")
 	public String index(Model model) {
-		List<Book> books = repository.findByTitle("Lord of the Flies");
 		
-		model.addAttribute("books", repository.findAll());
+		model.addAttribute("books", bookRepository.findAll());
 		
 		return "index";
 	}
@@ -35,14 +39,40 @@ public class BookController {
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
-		repository.save(book);
+		bookRepository.save(book);
 		return "redirect:index";
 	}
 	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-		repository.deleteById(bookId);
+		bookRepository.deleteById(bookId);
 		return "redirect:../index";
+	}
+	
+	@RequestMapping("/category")
+	public String category(Model model) {
+		
+		model.addAttribute("categories", categoryRepository.findAll());
+		
+		return "categorylist";
+	}
+	
+	@RequestMapping(value = "/addCategory")
+	public String addCategory(Model model) {
+		model.addAttribute("category", new Category());
+		return "addcategory";
+	}
+	
+	@RequestMapping(value = "/saveCategory", method = RequestMethod.POST)
+	public String saveCategory(Category category) {
+		categoryRepository.save(category);
+		return "redirect:category";
+	}
+	
+	@RequestMapping(value = "/deleteCategory/{id}", method = RequestMethod.GET)
+	public String deleteCategory(@PathVariable("id") Long categoryId, Model model) {
+		categoryRepository.deleteById(categoryId);
+		return "redirect:../category";
 	}
 	
 }
